2fe2441f019dff111f06a78a82e42057b7acaf8f
Tetiana Siestova <siestova.t@gmail.com>
2023-02-01 09:19:33 +0200

    project added

---
+++ README.md
@@ -0,0 +1,14 @@
+
+### Links
+
+- Live Site: [Demo]()
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS properties
+- Flexbox
+- Mobile-first workflow
+
+ 
+  
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      scriptLoading: "blocking",
+      inject: "head",
+    }),
+    new MiniCssExtractPlugin(),
+
+    new CopyPlugin({
+      patterns: [{ from: "./src/assets", to: "assets" }],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
